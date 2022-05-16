c0ba34c72b697851487f4d5ffb9a3997043eaf2e
Boris_Sadakov <veygard@gmail.com>
2022-05-16 14:43:53 +0300

    Исправление иконки приложения

--- app/src/main/java/com/veygard/starwarssage/util/ToastTypes.kt
+++ app/src/main/java/com/veygard/starwarssage/util/ToastTypes.kt
@@ -4,6 +4,7 @@ sealed class ToastTypes{
     object ConnectionError: ToastTypes()
     object ServerError: ToastTypes()
     object AppError: ToastTypes()
+    data class Download(val current:String, val all: String) : ToastTypes()
     data class RequestTimeOut(val pauseTime: Long): ToastTypes()
     object Loading: ToastTypes()
     object NoMoviesLeft: ToastTypes()
