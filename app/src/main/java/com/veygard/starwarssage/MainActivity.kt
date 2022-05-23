10ad45925da7f454a88a1cd5816ec7ba1960e8a7
Boris_Sadakov <veygard@gmail.com>
2022-05-23 11:35:20 +0300

    задержка по времени,чтобы показать SplashScreen

--- app/src/main/java/com/veygard/starwarssage/MainActivity.kt
+++ app/src/main/java/com/veygard/starwarssage/MainActivity.kt
@@ -3,6 +3,7 @@ package com.veygard.starwarssage
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.github.terrakok.cicerone.Command
 import com.github.terrakok.cicerone.Navigator
 import com.github.terrakok.cicerone.NavigatorHolder
@@ -10,11 +11,14 @@ import com.github.terrakok.cicerone.Replace
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import com.veygard.starwarssage.presentation.navigation.Screens
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    var showSplashScreen = true
 
     @Inject
     lateinit var navigatorHolder: NavigatorHolder
@@ -29,7 +33,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         title = getString(R.string.movies_fragment_title)
-        installSplashScreen()
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                showSplashScreen
+            }
+        }
+
+        //задержка по времени,чтобы показать SplashScreen
+        lifecycleScope.launch {
+            delay(1000)
+            showSplashScreen = false
+        }
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
